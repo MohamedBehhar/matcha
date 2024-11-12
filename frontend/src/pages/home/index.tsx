857d2ae9d0a3940efb0f5f15541e41b26e3c250a
MohamedBehhar <bahharmohamed18@gmail.com>
2024-11-12 22:46:26 +0100

    adding date picket and like on match making

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,13 +1,51 @@
 import useCountStore from "@/store/store";
 import { getUser } from "@/api/methods/user";
 import { useEffect } from "react";
+import { updateUserLocation } from "@/api/methods/user";
+import { DatePickerDemo } from "@/components/ui/datePicker";
 export default function HomePage() {
+  const id = localStorage.getItem("id");
   useEffect(() => {
     getUser().then((data) => console.log(data));
+    handleGetLocation();
   }, []);
+  const updateLocation = async (id: string, data: any) => {
+    try {
+      const response = await updateUserLocation(id, data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const handleGetLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          updateLocation(id || "", {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            userId: id,
+          });
+          setError(null);
+        },
+        (error) => {
+          setError("Unable to retrieve location.");
+        }
+      );
+    } else {
+      setError("Geolocation is not supported by this browser.");
+    }
+  };
+
   const count = useCountStore((state) => state.count);
   return (
     <main className="grid place-content-center">
+      <DatePickerDemo />
       <h1 className="text-6xl">Home Page</h1>
       {count}
     </main>
