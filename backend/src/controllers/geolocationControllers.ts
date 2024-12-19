c36c5f98dac3964dbbcbcd58556be67ff70844db
MohamedBehhar <bahharmohamed18@gmail.com>
2024-12-19 19:31:25 +0100

    fixing the error when no interests is selected

--- backend/src/controllers/geolocationControllers.ts
+++ backend/src/controllers/geolocationControllers.ts
@@ -8,13 +8,6 @@ class geolocationControllers {
 
   public async getUsersUnderRadius(req: Request, res: Response) {
     const { latitude, longitude, distance, user_id, max_age, min_age, interests } = req.query;
-    console.log('latitude', latitude);
-    console.log('longitude', longitude);
-    console.log('distance', distance);
-    console.log('user_id', user_id);
-    console.log('max_age', max_age);
-    console.log('min_age', min_age);
-    console.log('interests', interests);
     try {
       const users = await geolocationServices.getUsersUnderRadius(
         Number(latitude),
@@ -23,7 +16,7 @@ class geolocationControllers {
         user_id as string,
         Number(min_age),
         Number(max_age),
-        (interests as string).split(",")
+        interests ? (interests as string).split(",") : null
       );
       res.status(200).send(users);
     } catch (error) {
